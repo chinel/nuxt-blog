e718dee1a5a4f9ab4438c38b3196e6208db41780
chinel <nelokwaz@gmail.com>
2024-05-15 07:23:15 +0100

    fix: add async Data

--- pages/index.vue
+++ pages/index.vue
@@ -14,38 +14,43 @@ export default {
   components: {
     PostList,
   },
-  data() {
-    return {
-      loadedPosts: [
-        {
-          id: '1',
-          author: 'Ann',
-          title: 'This is my First Post',
-          thumbnailLink:
-            'https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/1015f/MainBefore.jpg',
-          previewText:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec nunc at tellus vehicula consequat.',
-        },
-        {
-          id: '2',
-          author: 'Ann',
-          title: 'This is my First Post',
-          thumbnailLink:
-            'https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/1015f/MainBefore.jpg',
-          previewText:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec nunc at tellus vehicula consequat.',
-        },
-        {
-          id: '3',
-          author: 'Ann',
-          title: 'This is my First Post',
-          thumbnailLink:
-            'https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/1015f/MainBefore.jpg',
-          previewText:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec nunc at tellus vehicula consequat.',
-        },
-      ],
-    }
+  asyncData(context, callback) {
+    // console.log(context)
+    // console.log('async data called-->')
+    // eslint-disable-next-line nuxt/no-timing-in-fetch-data
+    setTimeout(() => {
+      callback(null, {
+        loadedPosts: [
+          {
+            id: '1',
+            author: 'Ann',
+            title: 'This is my First Post',
+            thumbnailLink:
+              'https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/1015f/MainBefore.jpg',
+            previewText:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec nunc at tellus vehicula consequat.',
+          },
+          {
+            id: '2',
+            author: 'Ann',
+            title: 'This is my First Post',
+            thumbnailLink:
+              'https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/1015f/MainBefore.jpg',
+            previewText:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec nunc at tellus vehicula consequat.',
+          },
+          {
+            id: '3',
+            author: 'Ann',
+            title: 'This is my First Post',
+            thumbnailLink:
+              'https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/1015f/MainBefore.jpg',
+            previewText:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec nunc at tellus vehicula consequat.',
+          },
+        ],
+      })
+    }, 3000)
   },
 }
 </script>
